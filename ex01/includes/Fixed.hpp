d4ef83d1701d0a7209f565bb4e0d70977247b86b
RicoWD <pascual.machio.eric@gmail.com>
2026-01-14 02:51:07 +0100

    (ex01): Pending

--- ex01/includes/Fixed.hpp
+++ ex01/includes/Fixed.hpp
@@ -6,13 +6,35 @@
 /*   By: ep <ep@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 17:57:12 by ep                #+#    #+#             */
-/*   Updated: 2026/01/13 17:58:11 by ep               ###   ########.fr       */
+/*   Updated: 2026/01/13 20:53:10 by ep               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-# include <iostream>
-# include <iomanip>
-# include <string>
-# include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+
+class Fixed
+{
+    public:
+        Fixed();
+        Fixed(const Fixed &cpy);
+        Fixed &operator=(const Fixed &cpy);
+        Fixed(const int intToConvert);
+        Fixed(const float floatToConvert);
+        ~Fixed();
+
+        int   getRawBits() const;
+        void  setRawBits(int const raw);
+        float toFloat() const;
+        int   toInt() const;
+
+    private:
+        int              _fixedPointNbValue;
+        static const int _nbFractionalBits = 8;
+};
+
+std::ostream &operator<<(std::ostream &out, const Fixed &value);
---
+++ ex01/srcs/Fixed.cpp
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ep <ep@student.42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/13 19:40:35 by ep                #+#    #+#             */
+/*   Updated: 2026/01/13 20:47:48 by ep               ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+/* ************************************************************************** */																			   #
+// 																			  //
+// 						   ORTHODOX CANONICAL FORM							  //
+//																	 		  //
+/* ************************************************************************** */
+
+Fixed::Fixed() : _fixedPointNbValue(0)
+{
+	std::cout	<< "Default constructor called\n";
+}
+
+Fixed::Fixed(const Fixed &other)
+{
+	std::cout	<< "Copy constructor called\n";
+	*this = other;
+}
+
+Fixed &Fixed::operator=(const Fixed &other)
+{
+	std::cout	<< "Copy assignment operator called\n";
+	if (this != &other)
+		_fixedPointNbValue = other._fixedPointNbValue;
+	return *this;
+}
+
+Fixed::Fixed(const int intToConvert)
+{
+	std::cout	<< "Int constructor called\n";
+	_fixedPointNbValue = intToConvert << Fixed::_nbFractionalBits;
+}
+
+Fixed::Fixed(const float floatToConvert)
+{
+	std::cout	<< "Float constructor called\n";
+	_fixedPointNbValue = static_cast<int>(roundf(floatToConvert * (1 << Fixed::_nbFractionalBits)));
+}
+
+Fixed::~Fixed()
+{
+	std::cout	<< "Destructor called\n";
+}
+
+/* ************************************************************************** */																			   #
+// 																			  //
+// 									GET/SETERS								  //
+//																	 		  //
+/* ************************************************************************** */
+
+
+int 	Fixed::getRawBits() const
+{
+	std::cout	<< "getRawBits member function called\n";
+	return (this->_fixedPointNbValue);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	_fixedPointNbValue = raw;
+}
+
+
+/* ************************************************************************** */																			   #
+// 																			  //
+// 								MEMBER FUNCTION								  //
+//																	 		  //
+/* ************************************************************************** */
+
+float Fixed::toFloat(void) const
+{
+	return (static_cast<float>(this->_fixedPointNbValue) / (1 << Fixed::_nbFractionalBits));
+}
+
+int		Fixed::toInt(void) const
+{
+	return _fixedPointNbValue >> _nbFractionalBits;
+}
+
+/* ************************************************************************** */																			   #
+// 																			  //
+// 							  EXTERNAL FUNCTIONS							  //
+//																	 		  //
+/* ************************************************************************** */
+
+std::ostream &operator<<(std::ostream &out, const Fixed &value)
+{
+	out << value.toFloat();
+	return out;
+}
+
